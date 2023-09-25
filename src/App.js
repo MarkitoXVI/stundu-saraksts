6bb3a1a09924145c8d17588c909e21d39f35558f
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-25 14:49:10 +0300

    Pirmā stundu saraksta versija

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Diena from "./Diena";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hi mom</h1>
+      <Diena nosaukums="Pirmdiena"/>
+      <Diena nosaukums="Otrdiena"/>
+      <Diena nosaukums="Trešdiena"/>
+
+    </>
   );
 }
 
